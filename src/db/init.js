bd828d0f843b5cf0d4187ef6f1731e575b6e2ece
Leandro Arceno <leandroarcenoferreira@hotmail.com>
2021-04-13 21:49:30 -0300

    Initial commit

---
+++ src/db/init.js
@@ -0,0 +1,85 @@
+const Database = require('./config')
+
+const initDb = {
+    //é necessário criar uma estrutura com o async para que o await rode
+    async init() {
+
+
+        //await serve para aguardar o comando terminar de executar para continuar
+        const db = await Database()
+
+        //INTERGER é a mesma coisa que INT
+        await db.exec(`CREATE TABLE profile (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        avatar TEXT,
+        monthly_budget INT,
+        days_per_week INT,
+        hours_per_day INT,
+        vacation_per_year INT,
+        value_hour INT
+    )`)
+
+        await db.exec(`CREATE TABLE jobs(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        daily_hours INT,
+        total_hours INT,
+        created_at DATETIME
+    )`)
+
+        await db.run(`
+        INSERT INTO profile (
+            name,
+            avatar,
+            monthly_budget,
+            days_per_week,
+            hours_per_day,
+            vacation_per_year,
+            value_hour
+        )VALUES (
+            "Leandro",
+            "https://avatars.githubusercontent.com/u/58752588?v=4",
+            3000,
+            5,
+            5,
+            4,
+            70
+        );
+    `)
+
+        await db.run(`
+        INSERT INTO jobs (
+            name,
+            daily_hours,
+            total_hours,
+            created_at
+        )VALUES (
+            "Pizzaria Guloso",
+            2,
+            1,
+            1617514376018
+        );
+    `)
+
+        await db.run(`
+        INSERT INTO jobs (
+            name,
+            daily_hours,
+            total_hours,
+            created_at
+        )VALUES (
+            "OneTwo Projects",
+            3,
+            47,
+            1617514376018
+        );
+    `)
+
+
+        await db.close()
+    }
+
+}
+
+initDb.init()
